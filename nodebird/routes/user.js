39ea18f6a731ded4d446890bdef64f0137634af9
yoonsangkwak <yunsang08@naver.com>
2020-12-23 17:26:37 +0900

    unfollow update nickname

--- nodebird/routes/user.js
+++ nodebird/routes/user.js
@@ -20,4 +20,31 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
     }
 });
 
+router.post('/:id/unfollow', isLoggedIn, async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: { id: req.user.id } });
+        if (user) {
+            await user.removeFollowing(parseInt(req.params.id, 10));
+            res.send('success');
+        } else {
+            res.status(404).send('no user');
+        }
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.post('/profile', async (req, res, next) => {
+    try {
+        await User.update({ nick: req.body.nick }, {
+            where: { id: req.user.id },
+        });
+        res.redirect('/profile');
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+})
+
 module.exports = router;
