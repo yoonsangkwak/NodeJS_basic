9ea0355009a747d9df326507d7b81f035ffc9f92
yoonsangkwak <yunsang08@naver.com>
2020-12-26 20:45:25 +0900

    supertest

--- nodebird/controllers/user.test.js
+++ nodebird/controllers/user.test.js
@@ -1,6 +1,6 @@
 jest.mock('../models/user');
 const User = require('../models/user');
-const { addFollowing } = require('./user');
+const { addFollowing, removeFollowing } = require('./user');
 
 describe('addFollowing', () => {
     const req = {
@@ -34,6 +34,42 @@ describe('addFollowing', () => {
         const error = '테스트용 에러';
         User.findOne.mockReturnValue(Promise.reject(error));
         await addFollowing(req, res, next);
+        expect(next).toBeCalledWith(error);
+    });
+});
+
+describe('removeFollowing', () => {
+    const req = {
+        user: { id: 1 },
+        params: { id: 2 },
+    };
+    const res = {
+        status: jest.fn(() => res),
+        send: jest.fn(),
+    };
+    const next = jest.fn();
+
+    test('사용자를 찾아 팔로잉을 취소하고 success를 응답해야 함', async () => {
+        User.findOne.mockReturnValue(Promise.resolve({
+            removeFollowing(id) {
+                return Promise.resolve(true);
+            }
+        }));
+        await removeFollowing(req, res, next);
+        expect(res.send).toBeCalledWith('success');
+    });
+
+    test('사용자를 못 찾으면 res.status(404).send(no user)를 호출함', async () => {
+        User.findOne.mockReturnValue(null);
+        await removeFollowing(req, res, next);
+        expect(res.status).toBeCalledWith(404);
+        expect(res.send).toBeCalledWith('no user');
+    });
+
+    test('DB에서 에러가 발생하면 next(error) 호출함', async () => {
+        const error = '테스트용 에러';
+        User.findOne.mockReturnValue(Promise.reject(error));
+        await removeFollowing(req, res, next);
         expect(next).toBeCalledWith(error);
     });
 });
