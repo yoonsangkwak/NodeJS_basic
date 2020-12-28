6723b17b3bf4e484ca4c79bd819b02ea3105085f
yoonsangkwak <yunsang08@naver.com>
2020-12-28 14:11:16 +0900

    modify: ws -> socket.io

--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -1,26 +1,24 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
+    const io = SocketIO(server, { path: '/socket.io' });
 
-    wss.on('connection', (ws, req) => { // 웹 소켓 연결시
+    io.on('connection', (socket) => { // 웹 소켓 연결시
+        const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => { // 클라이언트로부터 메시지 수신시
-            console.log(message);
+        console.log('새로운 클라이언트 접속!', ip, socket.id, req.id);
+        socket.on('disconnect', () => { // 연결 종료시
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
         });
-        ws.on('error', (error) => { // 에러시
+        socket.on('error', (error) => { // 에러시
             console.error(error);
         });
-        ws.on('close', () => { // 연결 종료시
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
+        socket.on('reply', (data) => { // 클라이언트로부터 메시지 수신시
+            console.log(data);
         });
-
-        ws.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
-            if (ws.readyState === ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-            }
+        socket.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
+            socket.emit('news', 'Hello Socket.IO');
         }, 3000);
     });
 };
